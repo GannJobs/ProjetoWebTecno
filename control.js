486206beb4b4ddfd0e31b1041ce2e1d2d917ad91
GannJobs <linksyamato@gmail.com>
2022-06-18 17:41:05 -0300

    Alterei o sistema de movimento, inclui o sistema de limite de movimento, adicionei o turno, resta resolver as condições de vitoria e uma execção na movimentação, OBS: comentado a antiga solução ao final

--- control.js
+++ control.js
@@ -2,96 +2,46 @@ let matriz = document.querySelector(".Matriz")
 let casas = document.getElementsByClassName("Casa")
 let linhas = document.getElementsByClassName("Linha")
 let troca = document.getElementsByClassName("Pextra")
-let origem, origeml
+let origem
 
-let TurnV = 0, TurnP = 0
+let turn = 0
+
 
 for (casa of casas) {
     casa.addEventListener("click", event => {
         if (!origem) {
+
+            turn++
+
             origem = event.target
             origem.classList.add("ativo")
-            // if (origem.className.includes("Casa2")) {
-            //     TurnV++
-            // } else {
-            //     TurnP++
-            // }
-            // if (TurnP - TurnV > 1) {
-            //     TurnP--
-            //     TurnV++
-            //     origem.classList.remove("ativo")
-            //     origem = ""
-            //     return (alert("Nao jogue no turno do amiguinho #Respeito!"))
-            // } else if (TurnV - TurnP > 1) {
-            //     TurnP++
-            //     TurnV--
-            //     origem.classList.remove("ativo")
-            //     origem = ""
-            //     return (alert("Nao jogue no turno do amiguinho #Respeito!"))
-            // }
+            
+            Turn()
+
         } else {
 
             origem.classList.remove("ativo")
 
-            if (origem.id === event.target.id && !event.target.innerHTML) {
+            if (origem.id == event.target.id && event.target.innerHTML && event.target.innerHTML) {
+                origem = ""
+                return
+            }
+
+            if (origem.id == event.target.id && !event.target.innerHTML && !origem.innerHTML) {
                 sobrepor()
                 origem = ""
                 return
             }
 
-            // if (origem.id === event.target.id) {
-            //     if (origem.className.includes("Casa2"))
-            //         TurnV--
-            //     else
-            //         TurnP--
-            //     origem = ""
-            //     return
-            // }
-
             if (!origem.innerHTML) {
+
                 origem = ""
-                return alert("BUuurro!!!")
+                return alert("Não se pode passar nada para uma casa!")
+
             } else {
 
-                let l = linha(origem)
-                let l2 = linha(event.target)
-                if (l2 - l == 1 || l - l2 == 1) {
-                    if ((origem.innerHTML) && ((origem.id % 2 == 1) && (event.target.id % 2 == 1) && (event.target.innerHTML == 0))) {
-                        if (CondMovV(event)) {
-                            origem = ""
-                            return alert("para de ser burro!")
-                        } else {
-                            mov(event, "Verdes")
-                        }
-                    } else if ((origem.innerHTML) && (origem.id % 2 == 0) && (event.target.id % 2 == 0) && (event.target.innerHTML == 0)) {
-                        if (CondMovP(event)) {
-                            origem = ""
-                            return alert("para de ser burro!")
-                        } else {
-                            mov(event, "Pretas")
-                        }
-                    }
-                } else if ((origem.innerHTML) && l - l2 == 0 && ((origem.id - event.target.id == 1) * (origem.id - event.target.id == 1))) {
-                    if (origem.id - event.target.id < 2) {
-                        if (CondMovV(event)) {
-                            origem = ""
-                            return alert("para de ser burro!")
-                        } else {
-                            mov(event, "Verdes")
-                        }
-                        if ((origem.innerHTML) && (origem.id % 2 == 0) && (event.target.id % 2 == 0) && (event.target.innerHTML == 0)) {
-                            if (CondMovP(event)) {
-                                origem = ""
-                                return alert("para de ser burro!")
-                            } else {
-                                mov(event, "Pretas")
-                            }
-                        }
-                    }
-                } else {
-                    origem = ""
-                    return alert("para de ser burro!")
-                }
+                LimitMov(event)
+
             }
         }
     })
@@ -104,27 +54,194 @@ function sobrepor() {
     }
 }
 
-function CondMovP(evento) {
-    return ((origem.id == 11 && evento.target.id == 3) || (origem.id == 11 && evento.target.id == 13))
-        || ((origem.id == 7 && evento.target.id == 5) || (origem.id == 7 && evento.target.id == 15))
-        || ((origem.id == 3 && evento.target.id == 11) || (origem.id == 13 && evento.target.id == 11))
-        || ((origem.id == 5 && evento.target.id == 7) || (origem.id == 15 && evento.target.id == 7))
+function Turn() {
+    if (turn % 2 == 1) {
+        alert("Turno das verdes")
+        if (origem.className.includes("Casa2")) {
+            return
+        } else {
+            origem.classList.remove("ativo")
+            origem = ""
+            turn--
+            alert("E o turno das Verdes, nao pule o turno !")
+            return
+        }
+    } else if (turn % 2 == 0) {
+        alert("Turno das pretas")
+        if (!origem.className.includes("Casa2")) {
+            return
+        } else {
+            origem.classList.remove("ativo")
+            origem = ""
+            turn--
+            alert("E o turno das Pretas, nao pule o turno !")
+            return
+        }
+    }
 }
-
-function CondMovV(evento) {
-    return ((origem.id == 11 && evento.target.id == 3) || (origem.id == 11 && evento.target.id == 13))
-        || ((origem.id == 7 && evento.target.id == 5) || (origem.id == 7 && evento.target.id == 15))
-        || ((origem.id == 3 && evento.target.id == 11) || (origem.id == 13 && evento.target.id == 11))
-        || ((origem.id == 5 && evento.target.id == 7) || (origem.id == 15 && evento.target.id == 7))
-}
-
-
 
 function mov(evento, extra) {
     evento.target.innerHTML = origem.innerHTML
     origem.innerHTML = ""
     origem = null
     vencerV(extra)
+}
+
+function movV(evento) {
+    if ((evento.target.className.includes("Casa2") || (evento.target.className.includes("Casa sobreposto")))) {
+
+        mov(evento, "Verdes")
+
+    } else if (origem.className.includes("Casa sobreposto") && (evento.target.className.includes("Casa2")
+        || evento.target.className.includes("Casa sobreposto"))) {
+
+        mov(evento, "Verdes")
+
+    } else {
+        origem = ""
+        return alert("Movimento nao tratado!")
+    }
+}
+
+function movP(evento) {
+    if (origem.className.includes("Casa Casa2 sobreposto") || (!(evento.target.className.includes("Casa2"))
+        || evento.target.className.includes("Casa Casa2 sobreposto"))) {
+
+        mov(evento, "Pretas")
+
+    } else if (origem.className.includes("Casa Casa2 sobreposto") && (!evento.target.className.includes("Casa2")
+        || evento.target.className.includes("Casa Casa2 sobreposto"))) {
+
+        mov(evento, "Pretas")
+
+    } else {
+        origem = ""
+        return alert("Movimento nao tratado!")
+    }
+}
+
+function movimento(event) {
+    //Movimentação das verdes em casas normais e sobrepostas
+    if (origem.className.includes("Casa2") && event.target.className == "Casa Casa2") {
+
+        console.log("verde normal")
+        movV(event)
+
+    } else if ((origem.className.includes("Casa2") || (origem.className == "Casa sobreposto"))
+        && (event.target.className.includes("sobreposto"))) {
+
+        console.log("Verde => Preta->Verde")
+        movV(event)
+
+    } else if (origem.className == "Casa sobreposto" && ((event.target.className.includes("Casa2"))
+        || (event.target.className == "Casa sobreposto"))) {
+
+        console.log("Preta->Verde => Preta->Verde || Verde")
+        movV(event)
+
+    } else
+
+        //Movimentação das pretas em casas normais e sobrepostas
+        if (!origem.className.includes("Casa2") && event.target.className == "Casa") {
+
+            console.log("preta normal")
+            movP(event)
+
+        } else if ((!origem.className.includes("Casa2") || (origem.className == "Casa Casa2 sobreposto"))
+            && (event.target.className.includes("sobreposto"))) {
+
+            console.log("Preta => Verde->Preta")
+            movP(event)
+
+        } else if (origem.className == "Casa Casa2 sobreposto" && ((!event.target.className.includes("Casa2"))
+            || (event.target.className == "Casa Casa2 sobreposto"))) {
+
+            console.log("Verde->Preta => Verde->Preta || Preta")
+            movP(event)
+
+        } else {
+            origem = ""
+            alert("movimento Invalido!")
+        }
+}
+
+function LimitMov(evento) {
+
+    if (origem.id == 0 && (evento.target.id == 1 || evento.target.id == 2 || evento.target.id == 3)) {
+        movimento(evento)
+
+    } else if (origem.id == 1 && (evento.target.id == 0 || evento.target.id == 2 || evento.target.id == 4
+        || evento.target.id == 5 || evento.target.id == 6)) {
+        movimento(evento)
+
+    } else if (origem.id == 2 && (evento.target.id == 0 || evento.target.id == 1 || evento.target.id == 3
+        || evento.target.id == 5 || evento.target.id == 6 || evento.target.id == 7)) {
+        movimento(evento)
+
+    } else if (origem.id == 3 && (evento.target.id == 0 || evento.target.id == 2 || evento.target.id == 6
+        || evento.target.id == 7 || evento.target.id == 8)) {
+        movimento(evento)
+
+    } else if (origem.id == 4 && (evento.target.id == 1 || evento.target.id == 5 || evento.target.id == 9
+        || evento.target.id == 10)) {
+        movimento(evento)
+
+    } else if (origem.id == 5 && (evento.target.id == 1 || evento.target.id == 2 || evento.target.id == 4
+        || evento.target.id == 6 || evento.target.id == 9 || evento.target.id == 10 || evento.target.id == 11)) {
+        movimento(evento)
+
+    } else if (origem.id == 6 && (evento.target.id == 1 || evento.target.id == 2 || evento.target.id == 3
+        || evento.target.id == 5 || evento.target.id == 7 || evento.target.id == 10 || evento.target.id == 11
+        || evento.target.id == 12)) {
+        movimento(evento)
+
+    } else if (origem.id == 7 && (evento.target.id == 2 || evento.target.id == 3 || evento.target.id == 6
+        || evento.target.id == 8 || evento.target.id == 11 || evento.target.id == 12 || evento.target.id == 13)) {
+        movimento(evento)
+
+    } else if (origem.id == 8 && (evento.target.id == 3 || evento.target.id == 7 || evento.target.id == 12
+        || evento.target.id == 13)) {
+        movimento(evento)
+
+    } else if (origem.id == 9 && (evento.target.id == 4 || evento.target.id == 5 || evento.target.id == 10
+        || evento.target.id == 14)) {
+        movimento(evento)
+
+    } else if (origem.id == 10 && (evento.target.id == 4 || evento.target.id == 5 || evento.target.id == 6
+        || evento.target.id == 9 || evento.target.id == 11 || evento.target.id == 14 || evento.target.id == 15)) {
+        movimento(evento)
+
+    } else if (origem.id == 11 && (evento.target.id == 5 || evento.target.id == 6 || evento.target.id == 7
+        || evento.target.id == 10 || evento.target.id == 13 || evento.target.id == 14 || evento.target.id == 15
+        || evento.target.id == 16)) {
+        movimento(evento)
+
+    } else if (origem.id == 12 && (evento.target.id == 6 || evento.target.id == 7 || evento.target.id == 8
+        || evento.target.id == 11 || evento.target.id == 13 || evento.target.id == 15 || evento.target.id == 16)) {
+        movimento(evento)
+
+    } else if (origem.id == 13 && (evento.target.id == 7 || evento.target.id == 8 || evento.target.id == 12
+        || evento.target.id == 16)) {
+        movimento(evento)
+
+    } else if (origem.id == 14 && (evento.target.id == 9 || evento.target.id == 10 || evento.target.id == 11
+        || evento.target.id == 15 || evento.target.id == 17)) {
+        movimento(evento)
+
+    } else if (origem.id == 15 && (evento.target.id == 10 || evento.target.id == 11 || evento.target.id == 12
+        || evento.target.id == 14 || evento.target.id == 16 || evento.target.id == 17)) {
+        movimento(evento)
+
+    } else if (origem.id == 16 && (evento.target.id == 11 || evento.target.id == 12 || evento.target.id == 13
+        || evento.target.id == 15 || evento.target.id == 17)) {
+        movimento(evento)
+
+    } else if (origem.id == 17 && (evento.target.id == 14 || evento.target.id == 15 || evento.target.id == 16)) {
+        movimento(evento)
+
+    } else {
+        alert("Casa fora da vizinhaça!")
+    }
 }
 
 function vencerV(extra) {
@@ -171,43 +288,110 @@ function vencerV(extra) {
     }, 500);
 }
 
-function linha(a) {
-    if (a.classList == 'Casa Casa2') {
-        if (a.id == 1) {
-            return 1;
-        } else if (a.id >= 3 && a.id <= 5) {
-            return 2;
-        } else if (a.id >= 7 && a.id <= 11) {
-            return 3;
-        } else if (a.id >= 13 && a.id <= 15) {
-            return 4;
-        } else if (a.id == 17) {
-            return 5;
-        }
-    } else {
-        if (a.id == 2) {
-            return 2;
-        } else if (a.id >= 4 && a.id <= 6) {
-            return 3;
-        } else if (a.id >= 8 && a.id <= 12) {
-            return 4;
-        } else if (a.id >= 14 && a.id <= 16) {
-            return 5;
-        } else if (a.id == 18) {
-            return 6;
-        }
-    }
-}
 
-function isChild(linhas, origem) {
-    let childrenss = linhas.children
 
-    for (let i = 0; i < childrenss.length; i++) {
+// for (casa of casas) {
+//     casa.addEventListener("click", event => {
+//         if (!origem) {
+//             origem = event.target
+//             origem.classList.add("ativo")
+//             // if (origem.className.includes("Casa2")) {
+//             //     TurnV++
+//             // } else {
+//             //     TurnP++
+//             // }
+//             // if (TurnP - TurnV > 1) {
+//             //     TurnP--
+//             //     TurnV++
+//             //     origem.classList.remove("ativo")
+//             //     origem = ""
+//             //     return (alert("Nao jogue no turno do amiguinho #Respeito!"))
+//             // } else if (TurnV - TurnP > 1) {
+//             //     TurnP++
+//             //     TurnV--
+//             //     origem.classList.remove("ativo")
+//             //     origem = ""
+//             //     return (alert("Nao jogue no turno do amiguinho #Respeito!"))
+//             // }
+//         } else {
 
-        let pai = childrenss[i].children
-        let filho = document.getElementsByClassName(origem)
+//             origem.classList.remove("ativo")
 
-        if (pai.id == filho.id) return true
-        else return false
-    }
-}
+//             if (origem.id === event.target.id && !event.target.innerHTML) {
+//                 sobrepor()
+//                 origem = ""
+//                 return
+//             }
+
+//             // if (origem.id === event.target.id) {
+//             //     if (origem.className.includes("Casa2"))
+//             //         TurnV--
+//             //     else
+//             //         TurnP--
+//             //     origem = ""
+//             //     return
+//             // }
+
+//             if (!origem.innerHTML) {
+//                 origem = ""
+//                 return alert("BUuurro!!!")
+//             } else {
+
+//                 let l = linha(origem)
+//                 let l2 = linha(event.target)
+//                 if (l2 - l == 1 || l - l2 == 1) {
+//                     if ((origem.innerHTML) && ((origem.id % 2 == 1) && (event.target.id % 2 == 1) && (event.target.innerHTML == 0))) {
+//                         if (CondMovV(event)) {
+//                             origem = ""
+//                             return alert("para de ser burro!")
+//                         } else {
+//                             mov(event, "Verdes")
+//                         }
+//                     } else if ((origem.innerHTML) && (origem.id % 2 == 0) && (event.target.id % 2 == 0) && (event.target.innerHTML == 0)) {
+//                         if (CondMovP(event)) {
+//                             origem = ""
+//                             return alert("para de ser burro!")
+//                         } else {
+//                             mov(event, "Pretas")
+//                         }
+//                     }
+//                 } else if ((origem.innerHTML) && l - l2 == 0 && ((origem.id - event.target.id == 1) * (origem.id - event.target.id == 1))) {
+//                     if (origem.id - event.target.id < 2) {
+//                         if (CondMovV(event)) {
+//                             origem = ""
+//                             return alert("para de ser burro!")
+//                         } else {
+//                             mov(event, "Verdes")
+//                         }
+//                         if ((origem.innerHTML) && (origem.id % 2 == 0) && (event.target.id % 2 == 0) && (event.target.innerHTML == 0)) {
+//                             if (CondMovP(event)) {
+//                                 origem = ""
+//                                 return alert("para de ser burro!")
+//                             } else {
+//                                 mov(event, "Pretas")
+//                             }
+//                         }
+//                     }
+//                 } else {
+//                     origem = ""
+//                     return alert("para de ser burro!")
+//                 }
+//             }
+//         }
+//     })
+// }
+
+// function CondMovP(evento) {
+//     return ((origem.id == 11 && evento.target.id == 3) || (origem.id == 11 && evento.target.id == 13))
+//         || ((origem.id == 7 && evento.target.id == 5) || (origem.id == 7 && evento.target.id == 15))
+//         || ((origem.id == 3 && evento.target.id == 11) || (origem.id == 13 && evento.target.id == 11))
+//         || ((origem.id == 5 && evento.target.id == 7) || (origem.id == 15 && evento.target.id == 7))
+// }
+
+// function CondMovV(evento) {
+//     return ((origem.id == 11 && evento.target.id == 3) || (origem.id == 11 && evento.target.id == 13))
+//         || ((origem.id == 7 && evento.target.id == 5) || (origem.id == 7 && evento.target.id == 15))
+//         || ((origem.id == 3 && evento.target.id == 11) || (origem.id == 13 && evento.target.id == 11))
+//         || ((origem.id == 5 && evento.target.id == 7) || (origem.id == 15 && evento.target.id == 7))
+// }
+
